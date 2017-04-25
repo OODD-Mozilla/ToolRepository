75770de80651311c27d5fbe08164aa92e910ce06
Shashank Jha <shashankjha.np@gmail.com>
2017-04-25 15:01:46 -0400

    Add getOrgPullRequests util fucntion

--- utils/GitHubUtils.js
+++ utils/GitHubUtils.js
@@ -26,8 +26,25 @@ module.exports = {
 		});
 	}, 
 
-	//TODO
-	getOrgPullRequests: function(org, token, callback){
-		return null;
-	}
+	//to be used to get closed pull requests from specified repo
+	getOrgPullRequests: function(org, repo, token, callback){
+        //to be used while making http call
+        var options = {
+            url: githubUrlRoot + '/repos/' + org + '/' + repo + '/pulls?state=closed',
+            method: 'GET',
+            headers: {
+                "User-Agent": "EnableIssues",
+                "content-type": "application/json",
+                "Authorization": token
+            }
+        };
+
+        //make http call using request library
+        request(options, function(error, response, body) {
+            if (error) {
+                console.log("Error in getting all pull requests", error);
+            } else {
+                callback(JSON.parse(body));
+            }
+        });	}
 }
